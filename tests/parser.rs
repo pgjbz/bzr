315294e08481e0bc4efcff21443fe3173f5c2460
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2021-11-17 12:19:50 -0300

    Add suport to negative numbers and fix tests

--- tests/parser.rs
+++ tests/parser.rs
@@ -26,7 +26,7 @@ fn test_parse_var_int_type() {
 fn test_parse_let_negative_int_type() {
     let source = "let a int = -10;".to_string();
     let lexer = Lexer::new(Rc::new(source), Rc::new("foo.bzr".to_string()));
-    let parser = Parser::new(RefCell::new(lexer));
+    let parser = Parser::new(lexer);
     let progrma = parser.parse_program();
     assert!(progrma.errors.len() <= 0);
     assert!(progrma.statements.len() > 0);
@@ -36,7 +36,7 @@ fn test_parse_let_negative_int_type() {
 fn test_parse_var_negative_int_type() {
     let source = "var a int = -10;".to_string();
     let lexer = Lexer::new(Rc::new(source), Rc::new("foo.bzr".to_string()));
-    let parser = Parser::new(RefCell::new(lexer));
+    let parser = Parser::new(lexer);
     let progrma = parser.parse_program();
     assert!(progrma.errors.len() <= 0);
     assert!(progrma.statements.len() > 0);
@@ -66,7 +66,7 @@ fn test_parse_let_int_type_erros() {
 fn test_parse_var_int_letter_type_erros() {
     let source = "var a int = 10a".to_string();
     let lexer = Lexer::new(Rc::new(source), Rc::new("foo.bzr".to_string()));
-    let parser = Parser::new(RefCell::new(lexer));
+    let parser = Parser::new(lexer);
     let progrma = parser.parse_program();
     assert!(progrma.errors.len() > 0);
     assert!(progrma.statements.len() <= 0);
@@ -76,7 +76,7 @@ fn test_parse_var_int_letter_type_erros() {
 fn test_parse_let_int_letter_type_erros() {
     let source = "var a int = 10a".to_string();
     let lexer = Lexer::new(Rc::new(source), Rc::new("foo.bzr".to_string()));
-    let parser = Parser::new(RefCell::new(lexer));
+    let parser = Parser::new(lexer);
     let progrma = parser.parse_program();
     assert!(progrma.errors.len() > 0);
     assert!(progrma.statements.len() <= 0);
