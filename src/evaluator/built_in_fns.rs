24d8fbbe73efcea03a0a2f8f167d9904d7e5c06d
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2021-12-09 10:45:59 -0300

    Refactoring len function

--- src/evaluator/built_in_fns.rs
+++ src/evaluator/built_in_fns.rs
@@ -3,11 +3,14 @@ use std::rc::Rc;
 use crate::object::{integer::Integer, string::Str, Object};
 
 pub fn len(args: &[Rc<dyn Object>]) -> Rc<dyn Object> {
-    let mut buffer = String::new();
-    for arg in args {
-        buffer.push_str(&arg.to_string())
+    let mut len = 0;
+    if args.len() == 1 {
+        if let Some(string) = args[0].as_any().downcast_ref::<Str>() {
+            len = string.val.len()
+        } else {
+            len = args.len()
+        }
     }
-    let len = buffer.len();
     Rc::new(Integer::new(len as i64))
 }
 
